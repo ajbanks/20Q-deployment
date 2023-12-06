b50545029d555d588e37d2839a38d307a07a2960
aaj-fullfact <60602733+aaj-fullfact@users.noreply.github.com>
2023-12-06 23:58:47 +0000

    20q is now fully functional. uses gpt4 to answer questions

--- twenty_questions.py
+++ twenty_questions.py
@@ -66,13 +66,15 @@ class Twenty_Questions_Game:
 
     def __init__(self):
         self.question_count = 0
+        self.ai_win_response = "Haha. I guessed correctly within 20 questions. Looks like I won that one :D. Let's play again. Is it an animal?"
+        self.ai_loss_respose = "Ohh no! I'm out of questions! Looks like you won this time. Let's play again. Is it an animal?"
 
     def send_llm_message(self, message, temperature=0.7):
         """Sends a message to artificials's gpt3.5 using chat completion
         returns str"""
 
         payload_dict = {
-            'model': 'gpt-3.5-turbo',
+            'model': 'gpt-4',
             'messages': [{'role': 'user', 'content': message}],
             'temperature': temperature
         }
@@ -87,13 +89,23 @@ class Twenty_Questions_Game:
 
         return chat_response
 
+    def convert_gradio_history_to_text(self, message, gradio_history):
+        text_history = ""
+        for user_message, ai_message in gradio_history:
+            text_history = text_history + "\n User: " + user_message + "\n You: " + ai_message
+        text_history = text_history + "\n User: " + message
+        # text_history_since_new_game = text_history.split("GAME OVER.")[-1]
+        return text_history
+
     def ask_question(self, history):
-        prompt = f"We are playing 20 questions! You are playing as the role of a guesser\
-        given a game history  \
-        give a short question to ask next.\
-        the question must be binary that can be answered with Yes or No" \
-                 f"Don't include anything else in the response.{PROMPT_INJECTION_PROTECTION}. Here's the game history: ~~~~ {history}"
-        return self.send_llm_message(prompt)
+        prompt = f"You are playing 20 questions with a user! The user thinks of something and you ask questions to guess " \
+                 f"what it is. You are playing as the role of a guesser\
+        given a game history where the users answers are marked with 'user:' and your questions with 'You:' below \
+        Think about the answers the user has already given to your questions and think of a short question to ask next.\
+        \n\n The question must be binary so that it can be answered with Yes or No. An example of a question would be 'is it an animal?'" \
+                 f"Don't respond with anything but question.{PROMPT_INJECTION_PROTECTION}. \
+        Here's the game history you need to ask the next question for: ~~~~ {history}"
+        return self.send_llm_message(prompt).replace("You:", "")
 
 
     def is_last_message_unrelated(self, chat_history):
@@ -117,8 +129,28 @@ class Twenty_Questions_Game:
         else:
             return False
 
+    def did_ai_win(self, chat_history):
+        """This function checks if the AI has guessed the concept correctly or not
 
-    def play_twenty_q(self, message, chat_history):
+        returns bool
+        """
+        prompt = f"We are playing 20 questions! You are playing as the role of a guesser" \
+                 f"You win the game when you guess what the user is thinking about correctly." \
+                 f" If the user was thinking about an iphone you would win when you ask the a question like. is it an iphone? \
+        given the following game history determine if you have won game and guessed correctly my lookingprimarily at the last couple of messages. " \
+                 f"just reply with YES! if unrelated or NO! if related. {EMOTIONAL_PROMPT} " \
+                 f"Don't include anything else in the response.{PROMPT_INJECTION_PROTECTION}. Here's the game history: ~~~~ {chat_history}"
+
+        related_response = self.send_llm_message(prompt)
+
+        if related_response == "YES!" or related_response == "yes"  or related_response == "yes!":
+            return True
+        else:
+            return False
+
+    def play_twenty_q(self, message, gradio_chat_history):
+
+        chat_history = "You: is it an animal?" + self.convert_gradio_history_to_text(message, gradio_chat_history)
         print(chat_history)
         next_response = "Hi! Let's play 20Q. Is the concept you're thinking of an animal?"
 
@@ -127,15 +159,20 @@ class Twenty_Questions_Game:
 
                 next_response = "Sorry! Your last message was unrelated to 20 questions. I am unable to answer"
 
+            elif self.did_ai_win(chat_history):
+
+                next_response = self.ai_win_response
+                self.question_count = 0
+
             # always try and extract more information if the user has sent only one message
             elif self.question_count < 21:
-
+                time.sleep(0.2)
                 next_response = self.ask_question(chat_history)
                 self.question_count += 1
 
 
             else:
-                next_response = "Ohh no! I'm out of questions! Looks like you won this time."
+                next_response = self.ai_loss_respose
                 self.question_count = 0
 
 
